ead0fe938c230acfc0b327548489cda4abfc1f89
Foysal-Munsy <bdfoysal555@gmail.com>
2025-08-27 11:25:51 +0600

    hero structure added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+// Home Page
 import Image from "next/image";
+import Hero from "./Components/Hero";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Hero />
     </div>
   );
 }
